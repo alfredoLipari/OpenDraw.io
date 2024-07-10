07781316474cdc6d49997236f207eb3c5841b446
alipari <alfri.lipari@gmail.com>
2024-07-10 10:40:48 +0200

    axios base url correction 2

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "http://34.105.27.9:8080/",
+    baseURL: "http://34.105.27.9:8080/api/",
     headers: {
         "Content-Type": "application/json",
     },
