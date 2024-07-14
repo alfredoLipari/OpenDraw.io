317b5269940a2a5a1bef04b0bd7befc2689ab494
alipari <alfri.lipari@gmail.com>
2024-07-14 18:47:14 +0200

    add pdb

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "http://34.105.27.9:8080/api/",
+    baseURL: "http://34.154.202.90:8080/api/",
     //baseURL: "http://localhost:8000/",
     headers: {
         "Content-Type": "application/json",
